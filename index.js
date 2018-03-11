71fbdc3130be96cb9298a281d81c36a23febcf16
Mohammed Noori <kmanoorib@gmail.com>
2018-03-11 00:10:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function theBeatlesPlay (musicians, instruments) {
 }
 
 const facts = [
-  'He was the last Beatle to learn to drive',
+  "He was the last Beatle to learn to drive",
   'He was never a vegetarian',
   'He was a choir boy and boy scout',
   'He hated the sound of his own voice'
