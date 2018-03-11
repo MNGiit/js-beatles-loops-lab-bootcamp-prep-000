7be3de7c46fc852cf4a8b28e4f24f08935008c23
Mohammed Noori <kmanoorib@gmail.com>
2018-03-11 00:04:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+
+var array = [];
+
+function theBeatlesPlay (musicians, instruments) {
+  
+   for (var i = 0; i < 4; ++i){
+     
+     array.push(musicians[i] + " plays "+  instruments[i]);
+     
+   }
+  
+  return array;
+  
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+var strings = [];
+
+function johnLennonFacts (facts){
+  
+  for (var i = 0; i < 4; i++){
+    array.push(facts[i] +"!!!");
+  }
+  
+  return strings;
+}
